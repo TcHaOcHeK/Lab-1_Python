ac1dba4b621f3d942d675fc4d803f06853e8b407
TcHaOcHeK <160489385+TcHaOcHeK@users.noreply.github.com>
2024-03-11 19:03:02 +0600

    fixed some some some some some some bug

--- Pasrim.py
+++ Pasrim.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
 import requests # импортируем библиотеку requests
+import math
 
 def pars():
-    url = 'https://www.chitai-gorod.ru/' # передаем необходимы URL адрес
-    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
-    print(page.status_code) # смотрим ответ
-    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+    count = 0
+    end = []
+    description = { 'Name':[], 'Writer':[], 'Price':[]}
+    url = 'https://www.chitai-gorod.ru/search?phrase=python&page={}'# передаем необходимы URL адрес
+    while True:
+        count += 1
+        page = requests.get(url.format(count)) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+        print(page.status_code) # смотрим ответ
+        if page.status_code != 200:
+            break
 
-    block = soup.findAll('div', class_='slider__item') # находим  контейнер с нужным классом
+        soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+        block = soup.findAll('article') # находим  контейнер с нужным классом
+        for data in block: # проходим циклом по содержимому контейнера
+            description['Name'].append(data.find('div', class_='product-title__head').text.replace("\n", ""))
+            description['Writer'].append(data.find('div', class_='product-title__author').text.replace("\n", ""))
+            if data.find('div', class_='product-price__value product-price__value--discount'):
+                description['Price'].append(data.find('div', class_='product-price__value product-price__value--discount').text.replace("\n", ""))
+            else:
+                description['Price'].append("no prise")
 
-    description = { 'Name':['book'], 'Writer':['writer'], 'Price':['book prise']}
-    for data in block: # проходим циклом по содержимому контейнера
-        description['Name'].append(data.find('div', class_='product-title__head').text.replace("\n", ""))
-        description['Writer'].append(data.find('div', class_='product-title__author').text.replace("\n", ""))
-        description['Price'].append(data.find('div', class_='product-price__value product-price__value--discount').text)
+
     return description
